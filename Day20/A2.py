5fa8d747b91a15f4b8e688a1464c9d23e3615b00
veysi akin <weysi.akin1@gmail.com>
2020-12-31 15:21:47 +0100

    matplotlib pandas numpy

---
+++ Day20/A2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pylab as plt
+import numpy as np
+
+
+df = pd.read_csv('../Tag18/data/names.csv', index_col='Id')
+# Name Year Gender State Count
+
+# 1
+dff = df[df['Gender'] == 'F']
+dfm = df[df['Gender'] == 'M']
+
+f = dff['Name'].unique()
+m = dfm['Name'].unique()
+
+both = np.intersect1d(f, m)
+
+print(both)
+
+for name in both:
+    print(name)
